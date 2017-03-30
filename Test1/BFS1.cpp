7a76b97692ce5dd0e0b053c936226c4006ac5f7a
Chen <jingrui.chen@ucdconnect.ie>
2017-03-30 09:14:38 +0800

    3/30

--- Test1/BFS1.cpp
+++ Test1/BFS1.cpp
@@ -5,17 +5,17 @@ int n;
 int p[50][50];
 queue<int>x;
 queue<int>y;
-void push(int a, int b) {
+void push(int a, int b) {//检查四周是否能加入队列
 	if (a >= 0 && a <= n + 1 && b >= 0 && b <= n + 1) {
 		x.push(a);
 		y.push(b);
 	}
 	return;
 }
-void find1() {
+void find(int f) {//找到出现了f数字的位置并加入队列
 	for (int a = 1; a <= n; a++) {
 		for (int b = 1; b <= n; b++) {
-			if (p[a][b] == 0) {
+			if (p[a][b] == f) {
 				x.push(a);
 				y.push(b);
 				return;
@@ -24,34 +24,17 @@ void find1() {
 	}
 	return;
 }
-void c02() {
+void c(int s,int e) {//把s的数字换成e
 	int nx, ny;
 	while (1) {
 		nx = x.front();
 		ny = y.front();
-		if (p[nx][ny] == 0) {
-			p[nx][ny] = 2;
+		if (p[nx][ny] == s) {
+			p[nx][ny] = e;
 			push(nx, ny - 1);
 			push(nx, ny + 1);
 			push(nx + 1, ny);
 			push(nx - 1, ny);
-		}
-		x.pop();
-		y.pop();
-		if (x.empty() == 1)return;
-	}
-}
-void c01() {
-	int nx, ny;
-	while (1) {
-		nx = x.front();
-		ny = y.front();
-		if (p[nx][ny] == 0) { 
-			p[nx][ny] = 1;
-			push(nx , ny - 1);
-			push(nx , ny + 1);
-			push(nx + 1, ny );
-			push(nx - 1, ny );
 		}
 		x.pop();
 		y.pop();
@@ -74,12 +57,18 @@ int main() {
 	}
 	x.push(0);
 	y.push(0);
-	c01();
-	find1();
-	c02();
+	c(0,3);
+	find(0);
+	c(0,2);
+	//find(3);
+	//c(3,0);
 	for (int x = 1; x <= n ; x++) {
 		
 		for (int y = 1; y <= n; y++) {
+			if (p[x][y] == 3) {
+				printf("0 ");
+				continue;
+			}
 			printf("%d ", p[x][y]);
 		}
 		printf("\n");
---
+++ Test1/源1.cpp
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<algorithm>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+using namespace std;
+//void bsort(int *N, int n) {
+//    for (int x = 0; x<n - 1; x++) {
+//        for (int y = n - 2; y >= x; y--) {
+//            if (N[y + 1]<N[y])swap(N[y + 1], N[y]);
+//        }
+//    }
+//}
+void isort(int *N, int s, int e) {
+	for (int i = s + 1; i <= e; i++) {
+		int temp = i;
+		while (temp != s) {
+			if (N[temp] < N[temp - 1]) {
+				swap(N[temp], N[temp - 1]);
+			}
+			else break;
+			temp--;
+		}
+	}
+	return;
+}
+void qsort(int *N, int s, int e) {//array,start,end
+	if (s == e)return;
+	if (e - s <= 15) {
+		isort(N, s, e);
+		return;
+	}
+	int r = (int)(rand() % (e + 1 - s)) + s;//random base point
+											//printf("%s = %d%s", "rand = ", r, " ");
+	swap(N[s], N[r]);
+	int b = N[s];
+	int rs = e;//right search 
+	int ls = s;//left search
+	int k = s;
+	while (1) {//base number place one time
+		while (ls != rs&&N[rs]>b) {
+			rs--;
+		}
+		if (ls == rs)break;
+		N[k] = N[rs];
+		k = rs;
+		ls++;
+		//if (ls == rs)break; //Commented BB
+		while (ls != rs&&N[ls]<b) {
+			ls++;
+		}
+		if (ls == rs)break;
+		N[k] = N[ls];
+		k = ls;
+		rs--;
+		//if (ls == rs)break; //Commented BB
+	}
+	N[ls] = b;
+	if (ls == e) {
+		qsort(N, s, ls - 1);
+		return;
+	}
+	qsort(N, s, ls);
+	qsort(N, ls + 1, e);
+	return;
+}
+void mergearray(int *N, int s, int mid, int e, int *t) {
+	int s1 = s;
+	int s2 = mid+1;
+	int i = 0;
+	while (i<=e-s) {
+		if (N[s1] <= N[s2]) {
+			t[i++] = N[s1];
+			if(s1<mid)s1++;
+		}
+		else {
+			t[i++] = N[s2];
+			if (s2 < e)s2++;
+		}
+		for (int i = 0; i <= (e - s); i++) {
+
+	}
+	}
+}
+void msort(int *N,int s,int e,int *t) {
+	if (s < e) {
+		int mid = (s + e) / 2;
+		msort(N,s,mid,t);
+		msort(N, mid + 1, e, t);
+		mergearray(N, s, mid, e, t);
+	}
+}
+int main() {
+	srand((unsigned)time(NULL));
+	int n;
+	scanf("%d", &n);
+	int *number = (int*)malloc(sizeof(int)*n);
+	for (int i = 0; i<n; i++) {
+		scanf("%d", &number[i]);
+		//number[i] = 10;
+	}
+	//qsort(number, 0, n - 1);
+	int *t = new int[n];
+	if (t == NULL) {
+		printf("new fail");
+		return -1;
+	}
+		
+	msort(number, 0, n - 1,t);
+	for (int i = 0; i<n; i++) {
+		printf("%d ", number[i]);
+	}
+	printf("\n");
+}
