14224e7d7b689cf74899536852d9931b4602d765
Chen <jingrui.chen@ucdconnect.ie>
2017-08-08 12:23:23 +0800

    8/8

--- Test1/div.2 425 t4(lca).cpp
+++ Test1/div.2 425 t4(lca).cpp
@@ -27,10 +27,9 @@ struct NODE {
 	int last;
 }node[1000010];
 struct QUERY {
-	int to;
-	int last;
+	int to;			//to 表示路径/询问指向
+	int last;		//last 形成链表功能
 }search_[1000010];
-;
 int find(int i) {
 	if (parent[i] == i)return i;
 	else return parent[i] = find(parent[i]);
@@ -58,28 +57,49 @@ void tarjan(int u, NODE *node, QUERY *search, int from) {
 	}
 	for (int e = qhead[u]; e != 0; e = search_[e].last) {				//访问所有和u有询问关系的e
 		if (visited[search_[e].to] == 1) {
-			ans[e / 2] = find(search_[e].to);		//u, e的最近公共祖先为find(e);
+			//ans[e / 2] = find(search_[e].to);		//u, e的最近公共祖先为find(e);
+			ans[e / 2] = abs(rank_[find(search_[e].to)] - rank_[u]) + abs(rank_[find(search_[e].to)] - rank_[search_[e].to]);
 		}
 	}
 }
-int find_rank(int to,int from,int now_rank) {//预查询各节点rank
-	rank_[to] = now_rank;
-	for (int v = head[to]; v != 0; v = node[v].last) {	              
+void find_rank(int u, int from, int now_rank) {//预查询各节点rank
+	rank_[u] = now_rank;
+	for (int v = head[u]; v != 0; v = node[v].last) {
 		if (node[v].to == from)continue;
-		                
+		find_rank(node[v].to, u, now_rank + 1);
 	}
 }
 int main() {
 	input_int(n);
 	input_int(q);
+	for (int i = 1; i <= n; i++)parent[i] = i;
 	for (int i = 2; i <= n; i++) {
 		int t;
 		scanf("%d", &t);
-		add(t, i, i * 2 );
+		add(t, i, i * 2);
 		add(i, t, i * 2 + 1);
 	}
-	find_rank(1,1,1);
-		range0(i, q) {
-		
+	find_rank(1, 1, 1);
+	range0(i, q) {
+		input_int(a);
+		input_int(b);
+		input_int(c);
+		qadd(a, b, i * 6 + 6);
+		qadd(b, a, i * 6 + 1 + 6);
+		qadd(a, c, i * 6 + 2 + 6);
+		qadd(c, a, i * 6 + 3 + 6);
+		qadd(b, c, i * 6 + 4 + 6);
+		qadd(c, b, i * 6 + 5 + 6);
+		//get ans[3*i+3]\[3*i+4]\[3*i+5]
+	}
+	tarjan(1, node, search_, 1);
+	range0(i, q) {
+		sort(&ans[3 * i + 3], &ans[3 * i + 6]);
+		//printf("%d-%d-%d\n", ans[3 * i + 3], ans[3 * i + 4], ans[3 * i + 5]);
+		int dif = ans[3 * i + 5] - ans[3 * i + 4];
+		int t = ans[3 * i + 3] + dif;
+		t /= 2;
+		int a = ans[3 * i + 5] - t;
+		printf("%d\n", a+1);
 	}
 }
